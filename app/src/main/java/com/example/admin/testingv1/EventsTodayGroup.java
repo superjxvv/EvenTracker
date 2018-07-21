62c85de4082ce629bfbe2b3d973b39c3034a236a
LAPTOP-GJD5ANTD\Jeremy <j3r3my1996@gmail.com>
2018-07-22 01:52:03 +0800

    Settled the email in database

---
+++ app/src/main/java/com/example/admin/testingv1/EventsTodayGroup.java
@@ -0,0 +1,13 @@
+package com.example.admin.testingv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsTodayGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_today_group);
+    }
+}
